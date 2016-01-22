146ab6618b20fd8bff23f4420663ca91b8277fab
ses10 <sesma@live.com>
2016-01-22 15:49:35 -0500

    create master template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Master Template</div>
+            </div>
+        </div>
+    </body>
+</html>
