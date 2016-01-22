b3b50c4fb7fad6268969958df4c4ebca99352749
ses10 <sesma@live.com>
2016-01-22 17:04:01 -0500

    create nav bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,18 @@
     </head>
     <body>
         <div class="container">
+
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="#">Federal Employee Database</a>
+                </div>
+
+                <ul class="nav navbar-nav">
+                    <li><a href="#">All Departments</a></li>
+                    <li><a href="#">Add Employee</a></li>
+                </ul>
+            </nav>
+
             <div class="content">
                 <div class="title">Master Template</div>
             </div>
