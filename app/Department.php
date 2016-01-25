da2733092754383345fc1c6c133ad64c5597f9e1
ses10 <sesma@live.com>
2016-01-25 17:09:59 -0500

    create departmentManager()

--- app/Department.php
+++ app/Department.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+	
+	public static function departmentManager($deptNo){
+		return DB::table('employees')->join('dept_manager', 'dept_manager.emp_no', '=', 'employees.emp_no')
+									 ->select('employees.first_name', 'employees.last_name')
+									 ->where('dept_manager.dept_no', '=', $deptNo)
+									 ->where('dept_manager.to_date', '=', '9999-01-01')
+									 ->first();
+	}
 }
